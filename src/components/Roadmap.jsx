9a2c780fce065f638d2cc4244c742ae263341b00
PoluriLekhan <lekhanpoluri@gmail.com>
2025-07-06 06:45:21 +0530

    Update Roadmap.jsx

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -9,11 +9,11 @@ import { Gradient } from "./design/Roadmap";
 const Roadmap = () => (
   <Section className="overflow-hidden" id="roadmap">
     <div className="container md:pb-10">
-      <Heading tag="Ready to get started" title="What we’re building next" />
+      <Heading tag="Ready to get started" title="Our Services" />
 
       <div className="relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
         {roadmap.map((item) => {
-          const status = item.status === "done" ? "Done" : "In progress";
+          const status = item.status === "done" ? "Done" : "Done";
 
           return (
             <div
@@ -69,7 +69,7 @@ const Roadmap = () => (
       </div>
 
       <div className="flex justify-center mt-12 md:mt-15 xl:mt-20">
-        <Button href="/roadmap">Our roadmap</Button>
+        <Button href="#">Our Services</Button>
       </div>
     </div>
   </Section>
